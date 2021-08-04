8a1990e696af1145a9cee81096c6ed587338d9f2
luoheng <715493349@qq.com>
2021-08-04 11:59:31 +0800

    完成了登陆功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {Button,Form,FormItem,Input} from 'element-ui'
+// 导入单框提示组件  需要全局挂载
+import {Message} from "element-ui"
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+Vue.prototype.$message = Message
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from "@/components/Login"
+import Home from "@/components/Home"
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
 const router = new VueRouter({
-  routes
+    routes: [
+        /*让目录默认到login目录*/
+        {path: '/', redirect: '/login'},
+        {path: '/login', component: Login},
+        {path: '/home', component: Home}
+    ]
 })
 
+// 挂载路由导航守卫
+router.beforeEach((to,from,next) => {
+    // to 将要分访问路径
+    // from 代表从哪个路径跳转而来
+    // next 是一个函数，表示放行
+    //         next有两种调用方式：next() 放行     next('/login')强制跳转路径
+    // 判断用户访问的是不是登录页
+    if (to.path === '/login') return next()
+    // 获取token
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
+})
+
+// 退出功能实现原理
+// 局域token的方式实现退出比较简单，只需要销毁本地的token即可，这样，后续的请求就不会携带token,必须重新登陆生成一个新的token之后才可以访问页面
+            // 清空token    window.sessionStorage.clear()
+            // 跳转到登陆页      this.$router.push('/login')
 export default router
